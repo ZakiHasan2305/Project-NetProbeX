59477a4e0f749c80efbc7656207660eb74d3b431
clam433 <clam433@uwo.ca>
2024-02-02 19:03:45 -0500

    new changes

--- securitycheck.py
+++ securitycheck.py
@@ -1,82 +1,33 @@
-import pyshark
+from scapy.all import *
+from scapy.layers.inet import IP  # Add this line to import the IP class
 import math
-import collections
-from bytewax.dataflow import Dataflow, parse, spawn_cluster, AdvanceTo, Emit
-from securitycheck import Packets
 
-def input_builder(worker_index, worker_count):
-    # Since our input is not parallelizable, we open a live capture only
-    # in the first worker.
-    if worker_index == 0:
-        # Replace 'your_wifi_interface' with the appropriate Wi-Fi interface name
-        capture = pyshark.LiveCapture(interface='uwosecure-v2', bpf_filter="tcp")
-        epoch = 0
-        for packet in capture.sniff_continuously(packet_count=2000):
-            yield Emit(packet)
-            epoch += 1
-            yield AdvanceTo(epoch)
+def calculate_entropy(data):
+    if not data:
+        return 0
 
-def group_by_flow(packet):
-    '''packet flow in pyshark is shown as
-    the TCP stream'''
-    # protocol type
-    protocol = packet.transport_layer   
-    return packet[protocol].stream, packet
+    prob = [float(data.count(c)) / len(data) for c in set(data)]
+    entropy = - sum(p * math.log(p) / math.log(2.0) for p in prob)
+    return entropy
 
-flow = Dataflow() 
-#(epoch, packet_data)
-flow.map(group_by_flow)
-#(epoch, (flow_number, packet_data))
-flow.stateful_map(lambda key: Packets(), Packets.update)
+def analyze_packet(packet):
+    if IP in packet:
+        ip_src = packet[IP].src
+        ip_dst = packet[IP].dst
+        payload = packet[IP].payload
 
-class Packets:
-    def __init__(self):
-        self.count_seen_packets = 0
-        self.packet_counts_ = collections.defaultdict(int)
-        self.entropy = 0
+        if hasattr(payload, "load"):
+            data = payload.load
 
-    def update(self, packet):
-        self.protocol = packet.transport_layer   # protocol type
-        self.src_addr = packet.ip.src            # source address
-        self.src_port = packet[self.protocol].srcport   # source port
-        self.dst_addr = packet.ip.dst            # destination address
-        self.dst_port = packet[self.protocol].dstport   # destination port
-        self.packet_bytes = int(packet.length)   # bytes in the current packet
+            # Adjust the condition as needed for your specific case
+            if calculate_entropy(data) < 4.0:
+                print(f"Low entropy detected in packet from {ip_src} to {ip_dst} which is {calculate_entropy(data)}")
 
-        # calculate entropy of packet size
-        self.entropy = self._calc_entropy(self.packet_bytes, 1)
-
-        # update packet details
-        self.count_seen_packets += 1
-        self.packet_counts_[self.packet_bytes] += 1
-
-        return self, ((self.src_addr, self.src_port, self.dst_addr, self.dst_port), self.count_seen_packets, self.entropy)
-
-    def _calc_entropy(self, size_bytes, r):
-        '''Modified entropy calculation for estimating the incremental
-        entropy in a stream of data. This was modified from Blaz Sovdat's
-        paper - https://arxiv.org/abs/1403.6348 and the corresponding 
-        post -  https://stackoverflow.com/questions/48601396/calculating-incremental-entropy-for-data-that-is-not-real-numbers
-        '''
-        p_new = (self.packet_counts_[size_bytes] + 1) / (self.count_seen_packets + r)
-        p_old = self.packet_counts_[size_bytes] / (self.count_seen_packets + r)
-
-        residual = p_new * math.log2(p_new) - p_old * math.log2(p_old)
-        self.entropy = self.count_seen_packets * (self.entropy - math.log2(self.count_seen_packets / (self.count_seen_packets + r))) / (self.count_seen_packets + r) - residual
-
-        return self.entropy
-
-def alert_low_entropy(packet_data):
-    flow_number, data = packet_data
-    flow_details, num_packets, entropy = data
-    return entropy < 0.5 and num_packets > 4
-
-flow.filter(alert_low_entropy)
-
-def output_builder(worker_index, worker_count):
-    return print
+def analyze_pcap(file_path):
+    packets = rdpcap(file_path)
+    for packet in packets:
+        analyze_packet(packet)
 
 if __name__ == "__main__":
-    spawn_cluster(flow, input_builder, output_builder, **parse.cluster_args())
-    flow.inspect(print)
-    flow.capture()
+    pcap_file = "Project-NetProbeX/links.pcapng"
+    analyze_pcap(pcap_file)
