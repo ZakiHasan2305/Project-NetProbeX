f76c7bae6e12b24258ae9f5803ae82958c1b426c
clam433 <clam433@uwo.ca>
2024-02-02 19:44:56 -0500

    new email

--- sniffing.py
+++ sniffing.py
@@ -5,13 +5,18 @@ def packet_callback(pkt):
     if 'smtp' in str(pkt) or 'pop' in str(pkt) or 'imap' in str(pkt):
         print("Email-related packet found:")
         print(pkt)
+    else:
+        print("No Email-related packet was found, your network is secure")
 
 # Specify the path to your packet capture file
-file_path = r'Project-NetProbeX/links.pcapng'
+file_path = 'Project-NetProbeX//links.pcapng'
 
 # Open the file and set a callback function to be called for each packet
 capture = pyshark.FileCapture(file_path)
+
+print("Starting packet analysis...")
 capture.apply_on_packets(packet_callback)
+print("Packet analysis complete.")
 
 
 
