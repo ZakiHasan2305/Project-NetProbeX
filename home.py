451832e914ea32d3744495c070a24cf35857a17a
clam433 <clam433@uwo.ca>
2024-03-07 17:43:01 -0500

    Christopher's test

--- home.py
+++ home.py
@@ -14,7 +14,13 @@ from PySide6.QtCore import QTimer, Qt
 import pyqtgraph as pg
 from constants import wireshark_file_path
 from random import randint
-
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QListWidget, QProgressBar, QLabel, QListWidgetItem
+from PyQt5.QtGui import QPainter, QBrush, QColor
+from PyQt5.QtCore import Qt
+from securitycheck import get_packets_and_entropy
+from scapy.all import *
+from scapy.layers.inet import IP  # Add this line to import the IP class
+import math
 
 class Home(App):
     def build(self):
@@ -39,12 +45,17 @@ class Home(App):
         task1_label = Button(text='Task 1')
         task2_label = Button(text='Task 2')
         task3_label = Button(text='Task 3')
+        task4_label = Button(text='Task 4')
+        task5_label = Button(text='Task 5')
         
         task2_label.bind(on_press=self.open_visualaid)  # Binding the function to Task 2
+        task5_label.bind(on_press=self.filter_analysis_win)
         
         content.add_widget(task1_label)
         content.add_widget(task2_label)
         content.add_widget(task3_label)
+        content.add_widget(task4_label)
+        content.add_widget(task5_label)
         
         popup = Popup(title='Sidebar', content=content, size_hint=(None, 1), width=200)
         popup.open()
@@ -54,6 +65,12 @@ class Home(App):
             subprocess.Popen(['python', 'Project-NetProbeX/visualaid.py'])  # Launch visualaid.py script
        except Exception as e:
             print(f"Error occurred while launching visualaid.py: {e}")
+    
+    def filter_analysis_win(self, instance):
+        try:
+            subprocess.Popen(['python','Project-NetProbeX/filter_analysis_win.py'])
+        except Exception as e:
+            print(f"Error occured while launching filter_analysis_win.py: {e}")
 
 if __name__ == "__main__":
     Home().run()
